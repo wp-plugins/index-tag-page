e0100a904b85955b9958e862834b227cee275411
Mat_ <Mat_@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-05 20:32:25 +0000

    NEw version of index-tag-page plugin
    git-svn-id: https://plugins.svn.wordpress.org/index-tag-page/trunk@99887 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index-tag-page.php
@@ -0,0 +1,56 @@
+<?php   
+	/* 
+	Plugin Name: Index Tag Page 
+	Plugin URI: http://www.kune.fr 
+	Description: Plugin for displaying tag link index 
+	Author: Mat_ (Mathieu BASILI)
+	Version: 1.0 
+	Author URI: http://www.kune-studio.com 
+	*/  
+?>
+
+<?php 
+function tagIndex_init($atts) {
+	function tagIndexDisplay_function($atts) {
+		extract(shortcode_atts(array(
+			'nb' => '',
+			'ul' => ''
+		), $atts));
+		if($nb != '') $nb = "number=$nb&";
+		$tag = wp_tag_cloud($nb.'format=array&smallest=8&largest=8' );
+		$start = "0";
+		$ret = "";
+		if($ul != '') $ul = " class=\"$ul\" ";
+		foreach($tag as $untag){
+			
+			ereg(">([A-Za-z0-9\.|-|_éàèêç ]*)</a>",$untag, $letag);
+			
+			$letag[1] = ucfirst($letag[1]);
+
+			if($start == "0"){
+				$start = $letag[1][0];
+				$ret .= $start;
+				$ret .= "<ul $ul>";
+			}
+			if($letag[1][0] == $start)
+				$ret .= "<li>$untag</li>";
+			else{
+				$ret .= "</ul>";
+				$start = $letag[1][0];
+				$ret .= $start;
+				
+				 
+				
+				$ret .= "<ul>";
+				$ret .= "<li>$untag</li>";
+			}
+		}
+		
+		return $ret;
+	} 
+	
+	add_shortcode('indextag', 'tagIndexDisplay_function');
+}
+add_action('plugins_loaded', 'tagIndex_init');
+
+?>
