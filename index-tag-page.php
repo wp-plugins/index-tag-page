2587121e196414774b67a378981efd2206c594e7
Mat_ <Mat_@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-10 10:17:08 +0000

    git-svn-id: https://plugins.svn.wordpress.org/index-tag-page/branches/1.1@100959 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index-tag-page.php
@@ -0,0 +1,64 @@
+<?php   
+	/* 
+	Plugin Name: Index Tag Page 
+	Plugin URI: http://www.kune.fr 
+	Description: Plugin for displaying tag link index 
+	Author: Mat_
+	Version: 1.1 
+	Author URI: http://www.kune-studio.com 
+	*/  
+?>
+
+<?php 
+function tagIndex_init($atts) {
+	function tagIndexDisplay_function($atts) {
+		extract(shortcode_atts(array(
+			'nb' => '',
+			'ul' => '',
+			'letter' => ''
+		), $atts));
+		if($nb != '') $nb = "number=$nb&";
+		$tag = wp_tag_cloud($nb.'format=array&smallest=8&largest=8' );
+		$start = "0";
+		$ret = "";
+		if($ul != '') $ul = " class=\"$ul\" ";
+		else $ul = " class=\"itpUl\" ";
+		
+		if($li != '') $li = " class=\"$li\" ";
+		else $li = " class=\"itpLi\" ";
+		
+		foreach($tag as $untag){
+			
+			ereg(">([A-Za-z0-9\.|-|_éàèêç ]*)</a>",$untag, $letag);
+			
+			$letag[1] = ucfirst($letag[1]);
+
+			if($start == "0"){
+				$start = $letag[1][0];
+				if($letter != '') $letter = " class=\"$letter itp".$start." \" ";
+				else $letter = " class=\"itpLetter itp".$start." \" ";
+				$ret .= "<span ".$letter.">".$start."</span>";
+				$ret .= "<ul $ul>";
+			}
+			if($letag[1][0] == $start)
+				$ret .= "<li ".$li.">$untag</li>";
+			else{
+				$ret .= "</ul>";
+				$start = $letag[1][0];
+				$ret .= $start;
+				
+				 
+				
+				$ret .= "<ul>";
+				$ret .= "<li>$untag</li>";
+			}
+		}
+		
+		return $ret;
+	} 
+	
+	add_shortcode('indextag', 'tagIndexDisplay_function');
+}
+add_action('plugins_loaded', 'tagIndex_init');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Index tag page ===
+Contributors: Mat_
+Donate link: http://www.kune.fr/
+Tags: tag, page, index
+Requires at least: 2.0.2
+Tested up to: 2.7.1
+Stable tag: 1.0
+
+With this plugin, you will be able to display an index with all your tags, sort alphabeticaly.
+
+== Description ==
+
+With this plugin, you will be able to display an index with all your tags, sort alphabeticaly.
+The shortcode is `[indextag nb=XX ul=YYYY]`.
+
+XX is the number of tag that you want to display, YY is the class of the ul if you need it. 
+
+
+== Installation ==
+
+1. Upload `index-tag-page.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place `[indextag nb=XX ul=YYYY]` according to the description
+
+== Frequently Asked Questions ==
+
+= Is it work with Wordpress MU? =
+
+Of course it is.
+
+== Demonstration ==
+
+You can see an example of the functionnality of this plugin at [Kune.fr](http://kune.fr/index-des-tags/ "Kune.fr Tag Index Page")
+
+== Changes ==
+
+* 1.1 : Adding more options to the plugin
+* 1.0 : First release of the plugin
